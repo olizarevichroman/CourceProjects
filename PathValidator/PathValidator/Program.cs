7b84dd0725d50431f80ba5d7b08a579e05798e12
olizarevichroman <olizarevich97@mail.ru>
2018-01-07 01:37:04 +0300

    add all files

---
+++ PathValidator/PathValidator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter path: ");
+            string path = Console.ReadLine();
+            PathValidator validator = new PathValidator(path);
+            if (validator.IsPathValid())
+            {
+                Console.WriteLine("Path is valid");
+            }
+            else Console.WriteLine("Path is not valid");
+        }
+    }
+}
---
+++ PathValidator/PathValidatorTest/PathValidatorTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Data;
+using System.Data.Common;
+
+namespace Task1
+{
+    [TestClass]
+    
+    public class PathValidatorTests
+    {
+        public TestContext TestContext { get; set; }
+        
+        
+        
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+            "DataSource.xml",
+            "InvalidPath",
+            DataAccessMethod.Sequential)]
+        public void IsPathValidTest_InvalidPaths_returnedFalse()
+        {
+            string path = Convert.ToString(TestContext.DataRow["Path"]);
+            Assert.IsFalse(new PathValidator(path).IsPathValid());
+        }
+        
+       [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+            "DataSource.xml",
+            "ValidPath",
+            DataAccessMethod.Sequential)]
+        public void IsPathValidTest_ValidPaths_returnedTrue()
+        {
+            string path = Convert.ToString(TestContext.DataRow["Path"]);
+            Assert.IsTrue(new PathValidator(path).IsPathValid());
+        }
+    }
+}
